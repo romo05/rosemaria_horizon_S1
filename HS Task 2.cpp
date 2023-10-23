076d364115b7cdcee86ff7a5c37c97ac6e8c2195
romo05 <ro.maria5969@gmail.com>
2023-10-23 16:38:55 +0530

    Update HS Task 2.cpp

--- HS Task 2.cpp
+++ HS Task 2.cpp
@@ -13,9 +13,13 @@ int main()
     cout<<"Enter the velocity of the body(unit/s): ";
     cin>>ar[i][0];
     cout<<"Enter the direction of motion of body(degree):";
+    cout<<"\nEnter the velocity of the body(unit/s): ";
+    cin>>ar[i][0]; 
+    cout<<"Enter the direction of motion of body(degree): ";
     cin>>ar[i][1];
     cout<<"Enter the time interval (s): ";
     cin>>ar[i][2];
+    cin>>ar[i][2]; 
     }
     for(j=0; j<n; j++)
     {
@@ -36,7 +40,17 @@ int main()
             x=(float)x-x1;
             y=(float)y+y1;
         }    
-    }
     cout<<"\nPosition of body=("<<std::setprecision(5)<<x<<","<<std::setprecision(5)<<y<<")";
+        d=ar[j][0]*ar[j][2]; //distance = velocity * timeinterval
+        x1=(float)d*cos((ar[j][1])*(3.14/180)); //conversion of degree measure to radian measure
+        y1=(float)d*sin((ar[j][1])*(3.14/180)); //conversion of degree measure to radian measure
+        
+        /* Position of a particle in two dimensional space is given by x= dcos(theta) and y=dsin(theta)
+        where theta is the angle made with the positive direction of x axis*/
+        
+        x=(float)x+x1;
+        y=(float)y+y1;
+	}
+    cout<<"\nPosition of body=("<<std::setprecision(5)<<x<<","<<std::setprecision(4)<<y<<")";
     return 0;
 }//hello
