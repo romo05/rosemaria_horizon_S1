039c3d20f90dde9ee84901ffb85f60d92d32ac6f
romo05 <147421482+romo05@users.noreply.github.com>
2023-10-23 16:44:10 +0530

    Update HS Task 2.cpp

--- HS Task 2.cpp
+++ HS Task 2.cpp
@@ -10,38 +10,14 @@ int main()
     cin>>n;
     for(i=0; i<n; i++)
     {
-    cout<<"Enter the velocity of the body(unit/s): ";
-    cin>>ar[i][0];
-    cout<<"Enter the direction of motion of body(degree):";
     cout<<"\nEnter the velocity of the body(unit/s): ";
     cin>>ar[i][0]; 
     cout<<"Enter the direction of motion of body(degree): ";
     cin>>ar[i][1];
     cout<<"Enter the time interval (s): ";
     cin>>ar[i][2];
-    cin>>ar[i][2]; 
-    }
-    for(j=0; j<n; j++)
-    {
-        d=ar[j][0]*ar[j][2];
-        x1=(float)d*cos((ar[j][1])*(3.14/180));
-        //cout<<"\nx1= "<<x1;
-        y1=(float)d*sin((ar[j][1])*(3.14/180));
-        //cout<<"\ny1= "<<y1;
-        if((ar[j][1])*(3.14/180)>=0)
-        {
-            x=(float)x+x1;
-            //cout<<"\nx="<<x;
-            y=(float)y+y1;
-            //cout<<"\ny="<<y;
-        }    
-        else
-        {
-            x=(float)x-x1;
-            y=(float)y+y1;
-        }    
-    cout<<"\nPosition of body=("<<std::setprecision(5)<<x<<","<<std::setprecision(5)<<y<<")";
-        d=ar[j][0]*ar[j][2]; //distance = velocity * timeinterval
+    }    
+	d=ar[j][0]*ar[j][2]; //distance = velocity * timeinterval
         x1=(float)d*cos((ar[j][1])*(3.14/180)); //conversion of degree measure to radian measure
         y1=(float)d*sin((ar[j][1])*(3.14/180)); //conversion of degree measure to radian measure
         
@@ -53,4 +29,4 @@ int main()
 	}
     cout<<"\nPosition of body=("<<std::setprecision(5)<<x<<","<<std::setprecision(4)<<y<<")";
     return 0;
-}//hello
+}
